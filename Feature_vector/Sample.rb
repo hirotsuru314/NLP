4257d5f08a656a20ec477c5b3b29b1758ea90ba4
hirotsuru314 <hirofumi.tsuruta.314@gmail.com>
2016-07-20 16:03:57 +0000

    add Feature_vector

---
+++ Feature_vector/Sample.rb
@@ -0,0 +1,21 @@
+# _*_ coding: utf-8 _*_
+$:.unshift File.dirname(__FILE__)  # ロードパスにカレントディレクトリを追加
+require 'TfIdf'
+
+class FeatureVector
+  include TfIdf
+
+  def calculation(text)
+    split_words(text)
+    calculate_tf
+    calculate_tfidf
+    feature_vector
+  end
+end
+
+#ARGVで二つの文章を受け取り、計算を実行する。
+
+text = open(ARGV[0], &:read)
+
+test = FeatureVector.new
+test.calculation(text)
---
+++ Feature_vector/TfIdf.rb
@@ -0,0 +1,47 @@
+# _*_ coding: utf-8 _*_
+$:.unshift File.dirname(__FILE__)
+require 'natto'
+require "idf_dic"
+
+module TfIdf
+
+  #Mecabによる形態素解析（単語分割）
+  def split_words(text)
+    @arr = Array.new
+    nm = Natto::MeCab.new
+    nm.parse(text) do |n|
+      surface = n.surface
+      feature = n.feature.split(',')
+      #名詞のみを抽出する
+      if feature.first == '名詞' && feature.last != '*'
+        @arr.push(surface) #テキストを単語分割して配列で返す
+      end
+    end
+  end
+
+  #TF値を計算
+  #配列をkeyが形態素、valueがTF値(Float)のハッシュに変換
+  def calculate_tf
+    @tf = Hash.new
+    @arr.each do |word|
+      if(@tf.key?(word))
+        @tf[word] += 1
+      else
+        @tf[word] = 1
+      end
+    end
+    @tf.each do |key, value|
+      @tf[key] = value.to_f/@tf.size
+    end
+  end
+
+  def calculate_tfidf
+    @tfidf = Hash.new
+    @tf.each do |key, value|
+      if $idf.has_key?(key)
+        @tfidf[key] = $idf[key] * value
+      end
+    end
+    puts @tfidf
+  end
+end
